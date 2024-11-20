f6e03f76a308f7110f794d42d2ea219d2d24c696
Tiago <tiagocustodio@alunos.utfpr.edu.br>
2024-11-20 14:41:48 -0300

    Update version

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ FONT = pygame.font.Font(None, 36)
 
 # Inicialização
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Ship War 1.0")
+pygame.display.set_caption("ShipWar2)
 clock = pygame.time.Clock()
 
 # Fundo do jogo
